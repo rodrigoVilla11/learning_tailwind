f00e600cb7fa6fd631f5677824cc5bd60f7cf8e9
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-05-04 11:04:39 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    colors: {},
+    extend: {
+      backgroundImage: {
+        bali: "url('./public/img/bali.jpg')",
+        chicago: "url('./public/img/chicago.jpg')",
+        europe: "url('./public/img/europe.jpg')",
+        iceland: "url('./public/img/iceland.jpg')",
+        LA: "url('./public/img/LA.jpg')",
+        miami: "url('./public/img/miami.jpg')",
+        new_york: "url('./public/img/new_york.jpg')",
+        norway: "url('./public/img/norway.jpg')",
+        sanFrancisco: "url('./public/img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('./public/img/sanFranciscoDesktop.jpg')",
+        seattle: "url('./public/img/seattle.jpg')",
+        switzerland: "url('./public/img/switzerland.jpg')",
+        sydney: "url('./public/img/sydney.jpg')",
+        yosemite: "url('./public/img/yosemite.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+        white: "#FFFFFF",
+      }),
+      textColor: {
+        primary: "#CC2D4A",
+        secondary: "#8FA206",
+        terciary: "#61AEC9",
+        white: "#FFFFFF",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
